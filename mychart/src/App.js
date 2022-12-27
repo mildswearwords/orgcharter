0798edc93a39fb5fbbaae6137e9499d0f45bb469
mildswearwords <malcmurdoch@gmail.com>
2022-12-28 09:37:24 +1100

    Table functions
    Added some table functions to index

--- mychart/src/App.js
+++ mychart/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. OK.
         </p>
         <a
           className="App-link"
@@ -21,5 +22,72 @@ function App() {
     </div>
   );
 }
+function OrgChart(){
+	const svgRef = useRef();
+	useEffect((() => {
+		const svg = select(svgRef.current)
+			.append('svg')
+			.attr('width', 500)
+			.attr('height', 500);
+	}, []));
+	return (
+		<div ref = {svgRef}></div>
+	);
+}
+function orgTable(){
+	const [rows, setRows] = useState([
+		{id:1, name:'John Doe', title:'CEO', manager:''},
+		{id:2, name:'Jane Doe', title:'Chair', manager:''}
+	]);
+	const getUniqueNames = (rows) => {
+		const names = rows.map(row => row.name);
+		return [...new Set(names)];
+	}
+	const handleChange = (event, id) => {
+		const updatedRows = rows.map(row => {
+			if (row.id == id){
+				return {...row, [event.target.name]: event.target.value};
+			}
+			return row;
+		});
+		setRows(updatedRows);
+	}
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Title</th>
+					<th>Manager</th>
+				</tr>
+			</thead>
+			<tbody>
+				{rows.map(row => (
+					<tr key = {row.id}>
+						<td>
+							<input 
+								type = "text"
+								name = "name" 
+								value = {row.name} 
+								onChange = {event => handleChange(event, row.id)} 
+							/>
+						</td>
+						<td>
+							<input
+								type = "text"
+								name = "title"
+								value = {row.title}
+								onChange = {event => handleChange(event, row.id)}
+							/>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);}
+	
+								
+
+
 
 export default App;
--- mychart/src/index.js
+++ mychart/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import OrgChart from './App';
+import OrgTable from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+	<OrgTable />
   </React.StrictMode>
 );
+class ShoppingList extends React.Component {
+	render() {
+		return (
+			<div className="shopping-list">
+				<h1>Shopping List for {this.props.name}</h1>
+				<ul>
+					<li>Instagram</li>
+					<li>WhatsApp</li>
+					<li>Oculus</li>
+				</ul>
+			</div>
+		);
+	}
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
